afcf5e802624b94a8d544db0d51249fc52990efd
Guzmán285 <herberthguzman0@gmail.com>
2025-06-04 09:51:48 -0600

    inicio

---
+++ .sql
@@ -0,0 +1,11 @@
+CREATE DATABASE app03_haga
+
+create table clientes(
+id serial primary key not null,
+nombre varchar(50) not null,
+apellido varchar(50),
+telefeno int,
+nit varchar(15),
+correo varchar(15),
+situacion smallint default 1
+);
